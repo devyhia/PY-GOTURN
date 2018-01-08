561935fc18f6584e11246ae5d974a16736361943
Yehya Abouelnaga <devyhia@aucegypt.edu>
2018-01-08 06:11:28 +0000

    Switch PyTorch Model Dropout Back to x

--- train_tracker.sh
+++ train_tracker.sh
@@ -1,11 +1,11 @@
 IMAGENET_FOLDER='/home/devyhia/ILSVRC2014'
 ALOV_FOLDER='/home/devyhia/ALOV'
 PRE_MODEL='model_best_loss.pth'
-#--pretrained_model $PRE_MODEL \
 
 python -m goturn.train.train \
 --alov $ALOV_FOLDER \
 --imagenet $IMAGENET_FOLDER \
+--pretrained_model $PRE_MODEL \
 --lamda_shift 5 \
 --lamda_scale 15 \
 --min_scale -0.4 \
