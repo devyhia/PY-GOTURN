0d00c6bf9582c7a4bc0754f8d37b3955fb3cb704
Yehya Abouelnaga <devyhia@aucegypt.edu>
2018-01-08 06:17:18 +0000

    Change model in evaluate tracker

--- show_tracker_vot.sh
+++ show_tracker_vot.sh
@@ -1,5 +1,6 @@
 DEPLOY_PROTO='./nets/tracker.prototxt'		 
-CAFFE_MODEL='./nets/models/pretrained_model/tracker.caffemodel'
+#CAFFE_MODEL='./nets/models/pretrained_model/tracker.caffemodel'
+CAFFE_MODEL='./dump_model_iter_100000.caffemodel'
 TEST_DATA_PATH='/home/devyhia/VOT'		
 
 python -m goturn.test.show_tracker_vot \
